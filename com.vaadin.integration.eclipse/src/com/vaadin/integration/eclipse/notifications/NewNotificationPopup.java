4a4a55590f3e6178412b381ce8d848d64659098c
Denis Anisimov <denis@vaadin.com>
2015-10-20 14:23:23 +0300

    Updated layout for single notification.

--- com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/notifications/NewNotificationPopup.java
+++ com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/notifications/NewNotificationPopup.java
@@ -2,7 +2,6 @@ package com.vaadin.integration.eclipse.notifications;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
@@ -25,20 +24,10 @@ class NewNotificationPopup extends AbstractNotificationPopup {
     protected void createContentArea(Composite parent) {
         super.createContentArea(parent);
 
-        // composite below title
-        Composite pane = new Composite(parent, SWT.NONE);
-        GridLayout gridLayout = new GridLayout(1, false);
-        GridDataFactory.fillDefaults().grab(true, false)
-                .align(SWT.FILL, SWT.FILL).applyTo(pane);
-        pane.setLayout(gridLayout);
-        gridLayout.marginWidth = 0;
-
-        buildNotificationItem(pane);
-    }
-
-    private void buildNotificationItem(Composite pane) {
-        // TODO : make notification based on type and notification info
         SingleNotificationComposite control = new SingleNotificationComposite(
-                pane, notification, getManager());
+                parent, notification, getManager());
+        GridDataFactory.fillDefaults().grab(true, true).span(2, 1)
+                .align(SWT.FILL, SWT.FILL).applyTo(control);
     }
+
 }
--- com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/notifications/NotificationType.java
+++ com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/notifications/NotificationType.java
@@ -6,16 +6,21 @@ import com.vaadin.integration.eclipse.VaadinPlugin;
 
 enum NotificationType {
 
-    SIGN_IN(Utils.SIGN_IN_ICON), WEBINAR(null), VAADIN(
-            null);
+    SIGN_IN(Utils.SIGN_IN_ICON, null), WEBINAR(null, null), VAADIN(null, null);
     // TODO : more ?
     private final String iconId;
+    private final String backgroundId;
 
-    NotificationType(String iconId) {
+    NotificationType(String iconId, String backgroundId) {
         this.iconId = iconId;
+        this.backgroundId = backgroundId;
     }
 
     public Image getIcon() {
         return VaadinPlugin.getInstance().getImageRegistry().get(iconId);
+    }
+
+    public Image getBackground() {
+        return VaadinPlugin.getInstance().getImageRegistry().get(backgroundId);
     }
 }
