0d26de4a86d2bffe8853edeeacd0791881ca772b
Denis Anisimov <denis@vaadin.com>
2015-10-26 15:47:15 +0200

    Vaadin properties UI is redone to support custom UI widgets.

--- com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/preferences/VaadinPreferences.java
+++ com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/preferences/VaadinPreferences.java
@@ -1,11 +1,20 @@
 package com.vaadin.integration.eclipse.preferences;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.preference.BooleanFieldEditor;
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.mylyn.commons.ui.compatibility.CommonFonts;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
 
 import com.vaadin.integration.eclipse.VaadinPlugin;
 
@@ -13,29 +22,94 @@ import com.vaadin.integration.eclipse.VaadinPlugin;
  * The Eclipse preferences page for Vaadin plugin.
  */
 
-public class VaadinPreferences extends FieldEditorPreferencePage implements
-        IWorkbenchPreferencePage {
+public class VaadinPreferences extends PreferencePage
+        implements IWorkbenchPreferencePage {
+
+    private final List<VaadinFieldEditor> editors;
 
     public VaadinPreferences() {
-        super(GRID);
         setPreferenceStore(VaadinPlugin.getInstance().getPreferenceStore());
-    }
-
-    @Override
-    public void createFieldEditors() {
-        addField(new BooleanFieldEditor(
-                PreferenceConstants.UPDATE_NOTIFICATIONS_IN_NEW_PROJECTS,
-                "Enable Vaadin version update notifications in new projects",
-                getFieldEditorParent()));
-        addField(new BooleanFieldEditor(
-                PreferenceConstants.DISABLE_ALL_UPDATE_NOTIFICATIONS,
-                "Never inform of Vaadin version updates",
-                getFieldEditorParent()));
-        new Label(getFieldEditorParent(), SWT.LEFT)
-                .setText("If checked, notifications about new versions of Vaadin are not shown "
-                        + "even for projects that have these notifications enabled. In addition, no usage statistics will be collected. ");
+        editors = new ArrayList<VaadinFieldEditor>();
     }
 
     public void init(IWorkbench workbench) {
     }
+
+    @Override
+    public boolean performOk() {
+        for (VaadinFieldEditor editor : editors) {
+            editor.store();
+            editor.setPresentsDefaultValue(false);
+        }
+        return true;
+    }
+
+    @Override
+    protected Control createContents(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 1;
+        layout.marginHeight = 0;
+        layout.marginWidth = 0;
+        composite.setLayout(layout);
+        composite.setFont(parent.getFont());
+
+        createFieldEditors(composite);
+
+        createNotificationsSecton(composite);
+
+        // checkState();
+        return composite;
+    }
+
+    private void createFieldEditors(Composite composite) {
+        addField(new VaadinBooleanFieldEditor(
+                PreferenceConstants.UPDATE_NOTIFICATIONS_IN_NEW_PROJECTS,
+                "Enable Vaadin version update notifications in new projects",
+                composite));
+        addField(new VaadinBooleanFieldEditor(
+                PreferenceConstants.DISABLE_ALL_UPDATE_NOTIFICATIONS,
+                "Never inform of Vaadin version updates", composite));
+        new Label(composite, SWT.LEFT).setText(
+                "If checked, notifications about new versions of Vaadin are not shown "
+                        + "even for projects that have these notifications enabled.\nIn addition, no usage statistics will be collected. ");
+    }
+
+    private <T extends FieldEditor & VaadinFieldEditor> void addField(
+            T editor) {
+        editors.add(editor);
+        editor.setPage(this);
+        editor.setPreferenceStore(getPreferenceStore());
+        editor.load();
+    }
+
+    private void createNotificationsSecton(Composite composite) {
+        ExpandableComposite notificationsPanel = new ExpandableComposite(
+                composite, SWT.NONE, ExpandableComposite.TWISTIE
+                        | ExpandableComposite.CLIENT_INDENT);
+        // TODO : I18N
+        notificationsPanel.setText("Vaadin notifications");
+        notificationsPanel.setFont(CommonFonts.BOLD);
+    }
+
+    private interface VaadinFieldEditor {
+        void store();
+
+        void setPresentsDefaultValue(boolean booleanValue);
+    }
+
+    private static class VaadinBooleanFieldEditor extends BooleanFieldEditor
+            implements VaadinFieldEditor {
+
+        VaadinBooleanFieldEditor(String name, String label,
+                Composite composite) {
+            super(name, label, composite);
+        }
+
+        @Override
+        public void setPresentsDefaultValue(boolean booleanValue) {
+            super.setPresentsDefaultValue(booleanValue);
+        }
+    }
+
 }
